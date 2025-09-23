9d4c2aefdbcde5296e8f790e0c8701da685b1cb3
yasmine mohsen <yasminemohsen269@gmail.com>
2025-09-23 18:46:34 +0300

    primary Button and outlined Button

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,7 @@
+import 'package:financeapp/utils/app_color.dart';
+import 'package:financeapp/utils/app_theme.dart';
+import 'package:financeapp/widgets/primary_button.dart';
+import 'package:financeapp/widgets/primary_outlined_button_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
@@ -12,12 +16,68 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return ScreenUtilInit(
       designSize: const Size(375, 812),
+      builder: (context, child) {
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'Flutter Demo',
+          theme: AppTheme.lightTheme,
+          home: child,
+        );
+      },
+      child: MyHomePage(title: "title"),
+    );
+  }
+}
 
-      child: MaterialApp(
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("finance")),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            PrimaryButton(
+              text: "Login",
+              fontSize: 15.sp,
+              width: 330.w,
+              height: 5.h,
+              onPressed: () {},
+            ),
+            SizedBox(height: 20),
+
+            PrimaryOutlinedButtonWidget(
+              text: "Login",
+              fontSize: 15.sp,
+              width: 330.w,
+              height: 5.h,
+              onPressed: () {},
+            ),
+          ],
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
       ),
     );
   }
